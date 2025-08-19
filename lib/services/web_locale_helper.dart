c7ef453957a8a4a433f40b30c40cf38cf8898619
vzeman <vzeman@qualityunit.com>
2025-08-19 10:35:37 +0200

    fix: Suppress expected analyzer warnings for web-specific code
    Added ignore comments for intentional use of dart:html in web-specific
    locale detection. These warnings are expected since the file is only
    imported on web platforms via conditional imports.

--- lib/services/web_locale_helper.dart
+++ lib/services/web_locale_helper.dart
@@ -1,4 +1,5 @@
 // This file is only imported on web platforms
+// ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
 import 'dart:html' as html;
 import 'package:flutter/foundation.dart' show debugPrint;
 
